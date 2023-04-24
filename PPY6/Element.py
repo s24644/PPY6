1e100bc419a56b4e62f17feea8cfe6e6bf9313b5
s24644 <s24644@pjwstk.edu.pl>
2023-04-24 15:23:18 +0200

    rozpoczęte zadania

---
+++ PPY6/Element.py
@@ -0,0 +1,4 @@
+class Element:
+    def __init__(self, data=None, nextE=None):
+        self.data = data
+        self.nextE = nextE
---
+++ PPY6/MyLinkedList.py
@@ -0,0 +1,7 @@
+class MyLinkedList:
+    def __init__(self, head, tail, size):
+        self.head = head
+        self.tail = tail
+        self.size = size
+
+    def __str__(self):
